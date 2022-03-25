6dc2fd5fd1c2b0446a33b5b10526816b5aa7e661
sushantgwr87 <sushantgangwar87@gmail.com>
2022-03-25 16:40:33 +0530

    Updated App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import Tabs from "./component/Tabs";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
-    <div className="App">
-      <Tabs />
-    </div>
+    <>
+      <ToastContainer
+        position="top-center"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+      <div className="App">
+        <Tabs />
+      </div>
+    </>
   );
 }
 
