086ff2bdf20b44a38cb0963996e18283fee0abba
sushantgwr87 <sushantgangwar87@gmail.com>
2022-03-25 16:41:28 +0530

    Updated Helper Text Component

--- src/component/HelperText.jsx
+++ src/component/HelperText.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 
+export const HelperData = (errorData) => {
+    return <HelperText />
+}
+
 const HelperText = ({ error, text, id }) => {
 
-    const defaultText  = "Please Input "+ id;
+    const defaultText  = "Please Fill "+ id;
 
     return (
         <>
